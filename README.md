729c70a1a2d211a1d02e187136a0b573327d04c8
이지훈 <leejihoon@ijihun-ui-MacBookPro.local>
2021-01-20 18:45:41 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mju-apple-example
---
+++ main.js
@@ -0,0 +1,67 @@
+function gonews() {
+    window.location.href='https://www.mju.ac.kr/mjukr/255/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGbWp1a3IlMkYxNDElMkZhcnRjbExpc3QuZG8lM0Y%3D';
+}
+
+function gocommunity() {
+    window.location.href='https://www.mju.ac.kr/mjukr/310/subview.do';
+}
+
+function golibrary() {
+    window.location.href='https://lib.mju.ac.kr/index.ax';
+}
+
+function gocaminfo() {
+    window.location.href='http://iphak.mju.ac.kr/pages/?p=51&b=B_1_7&cate=%EC%9D%B8%EB%AC%B8%EC%BA%A0%ED%8D%BC%EC%8A%A4';
+}
+
+function gocamintro() {
+    window.location.href='https://www.mju.ac.kr/mjukr/193/subview.do';
+}
+
+function gombs() {
+    window.location.href='http://mjumbs.ac.kr/';
+}
+
+function goworkinfo() {
+    window.location.href='https://www.mju.ac.kr/mjukr/196/subview.do';
+}
+
+function gomyiweb() {
+    window.location.href='https://sso1.mju.ac.kr/login.do?redirect_uri=https://myiweb.mju.ac.kr/index_Myiweb.jsp';
+}
+
+function goeat() {
+    window.location.href='https://www.mju.ac.kr/mjukr/485/subview.do';
+}
+
+function gochatbot() {
+    window.location.href='http://answerny.ai/chatbot/projects/mju/chatbot_mju.html';
+}
+
+function govirtual() {
+    window.location.href='http://lms.mju.ac.kr/ilos/main/main_form.acl';
+}
+
+function goeclass() {
+    window.location.href='https://home.mju.ac.kr/user/index.action';
+}
+
+function gostudent() {
+    window.location.href='http://www.mju.ac.kr/mjukr/300/subview.do';
+}
+
+function gosupport() {
+    window.location.href='http://www.mju.ac.kr/mjukr/4766/subview.do';
+}
+
+function goface() {
+    window.location.href='https://www.facebook.com/MJUy48';
+}
+
+function goyou() {
+    window.location.href='https://www.youtube.com/channel/UCzYm_E3bph-TSMCnByPEwrg';
+}
+
+function goinsta() {
+    window.location.href='https://www.instagram.com/mju_run/';
+}
